538867f14bebd03a20a1e7037ba930e6763d60f9
nabaSarwar <naba.sarwar1@gmail.com>
2024-01-14 01:16:45 +0530

    created a flutter app

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'package:first_app/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+   const GradientContainer(this.color1, this.color2, {super.key});
+
+   const GradientContainer.purple({super.key})
+      : color1 = const Color.fromARGB(255, 237, 135, 255),
+        color2 = const Color.fromARGB(255, 246, 132, 170);
+
+  final Color color1;
+  final Color color2;
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
